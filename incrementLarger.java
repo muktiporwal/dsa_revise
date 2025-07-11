70eb3eed6927da0d791936c1881716ce06395a4f
Mukti Porwal <projectwork1290@gmail.com>
2025-07-11 22:13:11 +0530

    code

---
+++ incrementLarger.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class incrementLarger {
+    public static int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[n + 1];
+        result[0] = 1; 
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] digits = {9, 9, 9};
+        int[] result = plusOne(digits);
+        System.out.println(Arrays.toString(result));  
+    }
+}
